70903447d697f0f351b265140cadb79d0c341e82
Erhan <erhanilgar@gmail.com>
2023-07-14 20:15:43 +0100

    day 2 added

---
+++ src/test/java/com/eurotech/tests/day2_webdriver_basic/CallWebDriverFactory.java
@@ -0,0 +1,31 @@
+package com.eurotech.tests.day2_webdriver_basic;
+
+import com.eurotech.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class CallWebDriverFactory {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        /*
+        TC
+        navigates to http://eurotech.study/login
+        verify that title is DevEx
+         */
+
+        WebDriver driver = WebDriverFactory.getDriver("firefox");
+        driver.get("http://eurotech.study/login");
+
+        String expectedTitle = "DevEx";
+        String actualTitle = driver.getTitle();
+
+        if(expectedTitle.equals(actualTitle)){
+            System.out.println("Passed");
+        }else {
+            System.out.println("Failed");
+        }
+
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
