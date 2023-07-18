93f6b79cf7df51d8a59eeb7a4465f8fb7fa21309
Erhan <erhanilgar@gmail.com>
2023-07-18 20:33:36 +0100

    day 4 finished

---
+++ src/test/java/com/eurotech/tests/day4_basicLocators/HwBasicLocators.java
@@ -0,0 +1,16 @@
+package com.eurotech.tests.day4_basicLocators;
+
+public class HwBasicLocators {
+    public static void main(String[] args) {
+
+
+        /*
+        --open chrome browser
+        --navigate ebay.com
+        --search selenium
+        --verify that result area has selenium text
+         */
+
+
+    }
+}
