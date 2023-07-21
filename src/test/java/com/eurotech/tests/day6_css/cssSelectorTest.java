e7aef33a048720391af89763e576ae1fc9ed6054
ustazeynep <126025376+ustazeynep@users.noreply.github.com>
2023-07-21 21:18:18 +0100

    day6 homework added

---
+++ src/test/java/com/eurotech/tests/day6_css/cssSelectorTest.java
@@ -0,0 +1,15 @@
+package com.eurotech.tests.day6_css;
+
+public class cssSelectorTest {
+
+    /**
+     * Setup browser
+     * navigate to the "http://eurotech.study/login"
+     * locate email, passwrod, login button, confirm cookies button with CSS
+     * enter email, password and login
+     * close the browser
+     *
+     */
+
+
+}
