1f92361caf786c09374883ea8933a0dff2047efd
ustazeynep <126025376+ustazeynep@users.noreply.github.com>
2023-07-27 21:21:38 +0100

    Addition in DisplayedDemo class

--- src/test/java/com/eurotech/tests/day8_typeOfElements/DisplayedDemo.java
+++ src/test/java/com/eurotech/tests/day8_typeOfElements/DisplayedDemo.java
@@ -33,4 +33,33 @@ public class DisplayedDemo {
         Thread.sleep(2000);
         driver.close();
     }
+
+    @Test
+    public void test2() throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
+
+        //verify that Hello World! is not visible
+        // Here in this page, "Hello World" text is rendered after clicking the "Start Button",
+        // so it was not available neither in the html nor it was displayed initially.
+        // That is why is throwing "NoSuchElementException" when you try to locate it
+        WebElement helloWorld = driver.findElement(By.xpath("//h4[text()='Hello World!']"));
+        Assert.assertFalse(helloWorld.isDisplayed(),"Hello world is displayed");
+
+
+        //click start button
+        WebElement startBtn = driver.findElement(By.xpath("//div[@id='start']/button"));
+        startBtn.click();
+        Thread.sleep(6000);
+
+        //verify that Hello World! is visible (displayed)
+        Assert.assertTrue(helloWorld.isDisplayed(),"Hello World is NOT DISPLAYED");
+        System.out.println("helloWorld.getText() = " + helloWorld.getText());
+
+
+        Thread.sleep(2000);
+        driver.close();
+    }
 }
